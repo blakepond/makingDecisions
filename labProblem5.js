fe85128169d4e48ee8d88b96d34473d252786156
Blake Pond <blakehp@icloud.com>
2021-10-13 15:36:37 -0600

    labProblem5-7 complete

--- labProblem5.js
+++ labProblem5.js
@@ -88,3 +88,64 @@
 */
 
 //CODE HERE
+// for (let i = 1; i < 10; i++) {
+//     console.log("Hello")
+// }
+// ////////// PROBLEM 6 //////////
+
+/*
+  Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
+*/
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+////////// PROBLEM 7 //////////
+
+/*
+  Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+*/
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+let score = 0
+let passingScore = 7
+
+////////// PROBLEM 8 //////////
+
+/*
+  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+*/
+
+//CODE HERE
+
+////////// INTERMEDIATE PROBLEMS //////////
+
+////////// PROBLEM 9 //////////
+/*
+  Create a variable called 'changeMyMind' and set it equal to true. 
+  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+
+*/
+
+//CODE HERE
+
+////////// PROBLEM 10 //////////
+// Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
+
+//CODE HERE
+
+
+
+////////// ADVANCED PROBLEMS //////////
+
+////////// PROBLEM 11 //////////
+
+let z = 5
+// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+
+//CODE HERE
