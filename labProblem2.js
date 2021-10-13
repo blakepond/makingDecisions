a80d99377c4f7c8886aabedd8489a4a42b023aff
Blake Pond <blakehp@icloud.com>
2021-10-13 14:00:31 -0600

    labProblem2.js

---
+++ labProblem2.js
@@ -0,0 +1,21 @@
+/*
+  Create a variable called 'lovesCode' and set it equal to true. 
+  Check to see if 'lovesCode' is equal to true or false. 
+  If it is true, console log "I love to code!"
+  If it is not, console log "Coding has its challenges."
+*/
+
+// //CODE HERE
+// let lovesCode = true
+// if (lovesCode >= true) {
+//     console.log("I love to code!")
+// }
+// else if (lovesCode < true){
+//     console.log("Coding has its challenges.")
+// }
+// For problems 2-3 use the following lines of code:
+
+var amysAge = 29
+var brittanisAge = 34
+var amysBirthYear = 1991
+var brittanisBirthYear = 1986
