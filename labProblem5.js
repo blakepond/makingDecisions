ff290c7e08a4044c016a42f2e0d80a0b37fcf173
Blake Pond <blakehp@icloud.com>
2021-10-13 16:17:26 -0600

    labProblem11 complete

--- labProblem5.js
+++ labProblem5.js
@@ -154,14 +154,14 @@
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. 
 //It should log true. Hint: use the not operator.
 
-let changeMyMind = true
+// let changeMyMind = true
 
-if (changeMyMind !== true){
-    changeMyMind = true;
-} 
+// if (changeMyMind !== true){
+//     changeMyMind = true;
+// } 
         
     
-    console.log (changeMyMind)
+//     console.log (changeMyMind)
 
 
 
@@ -171,6 +171,14 @@ if (changeMyMind !== true){
 ////////// PROBLEM 11 //////////
 
 let z = 5
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// // Create a while loop that continues to run while z is greater than 0.
+//  Within the while loop, log a countdown from the value of z to 1. 
+//  Once you have logged the countdown, make sure you decrement z by 1. 
+//  If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+while (z > 0){
+    for (let i = z;i > 0;i--){
+        console.log(i)
+    }
+    z-=1;
+}
