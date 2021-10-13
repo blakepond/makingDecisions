03e79d07391117977bd5c7c4082809389662ab9e
Blake Pond <blakehp@icloud.com>
2021-10-13 14:23:30 -0600

    problem 2 solved

--- labProblem2.js
+++ labProblem2.js
@@ -5,17 +5,34 @@
   If it is not, console log "Coding has its challenges."
 */
 
-// //CODE HERE
+// // //CODE HERE
 // let lovesCode = true
-// if (lovesCode >= true) {
+// if (lovesCode === true) {
 //     console.log("I love to code!")
 // }
-// else if (lovesCode < true){
+// else if (lovesCode !== true){
 //     console.log("Coding has its challenges.")
 // }
-// For problems 2-3 use the following lines of code:
+// // For problems 2-3 use the following lines of code:
 
-var amysAge = 29
-var brittanisAge = 34
+var amysAge = 31
+var brittanisAge = 29
 var amysBirthYear = 1991
 var brittanisBirthYear = 1986
+////////// PROBLEM 2 //////////
+
+/*
+  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. 
+  Hint: Consider what will happen if they are the same age. Handle this situation.
+*/
+
+//CODE HERE
+if (amysAge < brittanisAge) {
+    console.log("Brittani is older!")
+}
+    else if (brittanisAge < amysAge) {
+        console.log("Amy is older!")
+    }
+    else {
+        console.log("They are the same age.")
+    }
